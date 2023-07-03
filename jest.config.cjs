573ba4ccd3c0ef9ff84b114d0219d6985c610e08
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-03 19:19:23 +0200

    chore(jest): add index.ts to ignore files

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
- preset: "jest-expo",
+  preset: "jest-expo",
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
-    ],
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"]
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
+  ],
+  coveragePathIgnorePatterns: ["index.ts"],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
 };
