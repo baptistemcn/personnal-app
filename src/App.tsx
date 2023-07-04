89734969a86c97d44e6defd65e9a92ec053bdb3e
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-04 21:34:22 +0200

    feat(card): create card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { Text } from "react-native";
-
-import { Wrapper } from "@components";
+import { Card, Wrapper } from "@components";
 
 export default function App() {
   return (
     <Wrapper>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Card />
     </Wrapper>
   );
 }
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export const Card = () => {
+  return <Text>Open up App.tsx to start working on your app!</Text>;
+};
