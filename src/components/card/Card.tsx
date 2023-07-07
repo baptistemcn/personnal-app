5fa819aed2134823ed7e5b1b95b16b3991f8dc8b
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-07 20:42:56 +0200

    feat(card): add title & text description

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -3,7 +3,12 @@ import { Box, Text } from "@theme";
 export const Card = () => {
   return (
     <Box>
-      <Text variant="title">Développeur Front-End</Text>
+      <Text variant="title1" padding="m">
+        Développeur Front-End
+      </Text>
+      <Text variant="text" padding="m">
+        Hey, je suis Baptiste Marcon, développeur basé en France 📍
+      </Text>
     </Box>
   );
 };
