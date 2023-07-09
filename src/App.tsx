8584866c1fb2ed57fbd16ac834e8a71e2363ea80
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-09 20:27:36 +0200

    chore(i18n): config & integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Card, Wrapper } from "@components";
 import { ThemeProvider } from "@theme";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n/i18n";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <Wrapper>
-        <Card />
-      </Wrapper>
+      <I18nextProvider i18n={i18n}>
+        <Wrapper>
+          <Card />
+        </Wrapper>
+      </I18nextProvider>
     </ThemeProvider>
   );
 }
---
+++ src/i18n/i18n.ts
@@ -0,0 +1,42 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import * as SecureStore from "expo-secure-store";
+import { getLocales } from "expo-localization";
+
+import * as fr from "./translations/fr.json";
+
+const locales = {
+  fr,
+};
+
+const DEFAULT_LOCALE = "fr";
+const defaultLanguage = getLocales()[0]?.languageCode || DEFAULT_LOCALE;
+
+const useLanguageStorage = {
+  type: "languageDetector",
+  async: true,
+  detect: async (callback: (arg0: string) => void) => {
+    const lang = await SecureStore.getItemAsync("language");
+    callback(lang || defaultLanguage);
+  },
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  init: () => {},
+  cacheUserLanguage: async (language: string) => {
+    SecureStore.setItemAsync("language", language);
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .use(useLanguageStorage)
+  .init({
+    fallbackLng: DEFAULT_LOCALE,
+    compatibilityJSON: "v3",
+    resources: locales,
+    debug: false,
+    react: {
+      useSuspense: false,
+    },
+  });
+
+export default i18n;
