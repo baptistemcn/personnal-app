86ac9d811def4ca44eb5713e8c7895835c4705cd
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-16 18:39:15 +0200

    style(wrapper): add marin vertical

--- src/components/wrapper/Wrapper.spec.tsx
+++ src/components/wrapper/Wrapper.spec.tsx
@@ -1,6 +1,15 @@
 import { render } from "@testing-library/react-native";
 import { Wrapper } from "./Wrapper";
 
+jest.mock("@shopify/restyle", () => {
+  const RealModule = jest.requireActual("@shopify/restyle");
+  const RN = jest.requireActual("react-native");
+  RealModule.createText = () => RN.Text;
+  RealModule.createBox = () => RN.View;
+  RealModule.createRestyleComponent = (f: unknown, c: unknown) => c || RN.View;
+  return RealModule;
+});
+
 describe("Wrapper component", () => {
   const wrapperMock = "S-CREW";
   it("should render", () => {
--- src/components/wrapper/Wrapper.tsx
+++ src/components/wrapper/Wrapper.tsx
@@ -3,7 +3,13 @@ import { WrapperProps } from "@types";
 
 export const Wrapper = ({ children, testID = "wrapper" }: WrapperProps) => {
   return (
-    <Box flex={1} alignContent={"center"} alignItems={"center"} testID={testID}>
+    <Box
+      flex={1}
+      marginVertical={"m"}
+      alignContent={"center"}
+      alignItems={"center"}
+      testID={testID}
+    >
       {children}
     </Box>
   );
