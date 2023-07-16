ddd39690083a2c9ad98fabf3e360ff0b3d0498e2
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-16 12:19:03 +0200

    feat(about): add scroll view

--- src/screens/about/About.tsx
+++ src/screens/about/About.tsx
@@ -1,22 +1,25 @@
-import { Experience, Wrapper } from "@components";
-
-import { Box, Text } from "@theme";
 import { useTranslation } from "react-i18next";
+
+import { Experience, Wrapper } from "@components";
+import { Box, Text } from "@theme";
+import { ScrollView } from "react-native";
 
 export const About = () => {
   const { t } = useTranslation();
 
   return (
-    <Wrapper>
-      <Text variant={"title1"} padding={"l"} testID="title">
-        {t("about.title")}
-      </Text>
-      <Text variant={"text"} padding={"l"} testID="text">
-        {t("about.text")}
-      </Text>
-      <Box>
-        <Experience />
-      </Box>
-    </Wrapper>
+    <ScrollView>
+      <Wrapper>
+        <Text variant={"title1"} padding={"l"} testID="title">
+          {t("about.title")}
+        </Text>
+        <Text variant={"text"} padding={"l"} testID="text">
+          {t("about.text")}
+        </Text>
+        <Box>
+          <Experience />
+        </Box>
+      </Wrapper>
+    </ScrollView>
   );
 };
