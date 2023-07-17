f2dc5586947fec2abd57e9718f58c97d142d4044
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-17 22:27:55 +0200

    feat(certifications): add title & certificate component

--- src/screens/certifications/Certifications.tsx
+++ src/screens/certifications/Certifications.tsx
@@ -1,5 +1,18 @@
-import { Text } from "@theme";
+import { Certificate, Wrapper } from "@components";
+import { Box, Text } from "@theme";
+import { ScrollView } from "react-native";
 
 export const Certifications = () => {
-  return <Text>Certifications</Text>;
+  return (
+    <ScrollView>
+      <Wrapper>
+        <Text variant={"title1"} marginVertical={"l"} testID="title">
+          Certifications
+        </Text>
+        <Box marginVertical={"l"}>
+          <Certificate />
+        </Box>
+      </Wrapper>
+    </ScrollView>
+  );
 };
