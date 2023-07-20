08d938b576e6f7df1323f9e8b160f86c41b386f4
Baptiste Marcon <baptistemcn@icloud.com>
2023-07-20 22:57:25 +0200

    feat(projects): add translation title

--- src/screens/projects/Projects.tsx
+++ src/screens/projects/Projects.tsx
@@ -1,15 +1,17 @@
 import { ScrollView } from "react-native";
+import { useTranslation } from "react-i18next";
 
 import { PROJECTS } from "@assets";
 import { Project, Wrapper } from "@components";
 import { Box, Text } from "@theme";
 
 export const Projects = () => {
+  const { t } = useTranslation();
   return (
     <ScrollView>
       <Wrapper>
         <Text variant={"title1"} marginVertical={"l"}>
-          Projects
+          {t("projects.title")}
         </Text>
         <Box marginVertical={"l"}>
           <Project project={PROJECTS.reverse()} />
