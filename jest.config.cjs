d08b330f795b594e616a6d3aa099272b696450b8
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-04 22:28:06 +0200

    chore(jest): add api folder ignore

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,6 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
   ],
-  coveragePathIgnorePatterns: ["db.ts", "index.ts", "Theme.tsx"],
+  coveragePathIgnorePatterns: ["db.ts", "index.ts", "Theme.tsx", "src/api"],
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
 };
