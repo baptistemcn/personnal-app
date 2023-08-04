0e24efd2fa4ddbd61d32e9571dd9bde4a6b95045
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-04 23:29:39 +0200

    test(spinner): add spinner unit tests

---
+++ src/components/spinner/Spinner.spec.tsx
@@ -0,0 +1,57 @@
+import { render } from "@testing-library/react-native";
+import { Spinner } from "./Spinner";
+
+describe("Spinner component", () => {
+  const mockSpinner = {
+    color: "FF0000",
+    size: 256,
+  };
+
+  it("should render", () => {
+    render(<Spinner />);
+  });
+
+  it("should render a spinner with a default color", () => {
+    const { getByTestId } = render(<Spinner />);
+
+    const spinner = getByTestId("spinner");
+
+    expect(spinner).toBeTruthy();
+    expect(spinner).toBeOnTheScreen();
+
+    expect(spinner.props.color).toEqual("000000");
+  });
+
+  it("should render a spinner with a defined color", () => {
+    const { getByTestId } = render(<Spinner {...mockSpinner} />);
+
+    const spinner = getByTestId("spinner");
+
+    expect(spinner).toBeTruthy();
+    expect(spinner).toBeOnTheScreen();
+
+    expect(spinner.props.color).toEqual(mockSpinner.color);
+  });
+
+  it("should render a spinner with default size", () => {
+    const { getByTestId } = render(<Spinner />);
+
+    const spinner = getByTestId("spinner");
+
+    expect(spinner).toBeTruthy();
+    expect(spinner).toBeOnTheScreen();
+
+    expect(spinner.props.size).toEqual("small");
+  });
+
+  it("should render a spinner with defined size", () => {
+    const { getByTestId } = render(<Spinner {...mockSpinner} />);
+
+    const spinner = getByTestId("spinner");
+
+    expect(spinner).toBeTruthy();
+    expect(spinner).toBeOnTheScreen();
+
+    expect(spinner.props.size).toEqual(mockSpinner.size);
+  });
+});
