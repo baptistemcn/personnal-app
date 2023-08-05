ef1f0fe20e3d436ac0eb60166ecbacad21bb0626
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-05 21:29:28 +0200

    feat(projects): add api results & error handling

--- src/screens/projects/Projects.spec.tsx
+++ src/screens/projects/Projects.spec.tsx
@@ -1,5 +1,7 @@
-import { render } from "@testing-library/react-native";
+import { render, waitFor } from "@testing-library/react-native";
 import { Projects } from "./Projects";
+
+import * as api from "../../api/projects";
 
 jest.mock("@shopify/restyle", () => {
   const RealModule = jest.requireActual("@shopify/restyle");
@@ -19,18 +21,26 @@ jest.mock("react-i18next", () => ({
   }),
 }));
 
-const mockProject = [
-  {
-    title: "Spotify Profile",
-    description: "Play with Spotify API",
-    host: {
-      name: "Spotify Profile",
-      url: "https://github.com/baptistemcn/spotify-profile/",
-    },
-  },
-];
-
 describe("Projects screen", () => {
+  const mockProject = [
+    {
+      title: "Project 1",
+      description: "Project Description 1",
+      host: {
+        name: "Project name 1",
+        url: "https://github.com/project/1",
+      },
+    },
+    {
+      title: "Project 2",
+      description: "Project Description 2",
+      host: {
+        name: "Project name 2",
+        url: "https://github.com/project/2",
+      },
+    },
+  ];
+
   it("should render", () => {
     render(<Projects />);
   });
@@ -44,14 +54,47 @@ describe("Projects screen", () => {
     expect(titleElement).toBeOnTheScreen();
   });
 
-  it("shoud render a list of projects", () => {
+  it("shoud render a list of projects", async () => {
+    jest.spyOn(api, "getProjects").mockResolvedValue(mockProject);
+
+    const { getByTestId, getByText } = render(<Projects />);
+
+    const component = getByTestId("projects");
+    const spinner = getByTestId("spinner");
+
+    expect(component).toBeTruthy();
+    expect(component).toBeOnTheScreen();
+
+    expect(spinner).toBeTruthy();
+    expect(spinner).toBeOnTheScreen();
+
+    await waitFor(() => {
+      expect(spinner).not.toBeOnTheScreen();
+    });
+
+    expect(getByText(mockProject[0].title)).toBeOnTheScreen();
+    expect(getByText(mockProject[1].title)).toBeOnTheScreen();
+  });
+
+  it("should render error if no api response", async () => {
+    jest
+      .spyOn(api, "getProjects")
+      .mockRejectedValue(new Error("Failed to fetch data"));
+
     const { getByTestId } = render(<Projects />);
 
-    const projectsElement = getByTestId("projects");
+    const component = getByTestId("projects");
 
-    expect(projectsElement).toBeTruthy();
-    expect(projectsElement).toBeOnTheScreen();
+    const spinner = getByTestId("spinner");
 
-    expect(projectsElement.props.children.props.project).toEqual(mockProject);
+    expect(component).toBeTruthy();
+    expect(component).toBeOnTheScreen();
+
+    expect(spinner).toBeTruthy();
+    expect(spinner).toBeOnTheScreen();
+
+    await waitFor(() => {
+      expect(spinner).toBeOnTheScreen();
+    });
   });
 });
--- src/screens/projects/Projects.tsx
+++ src/screens/projects/Projects.tsx
@@ -1,12 +1,33 @@
 import { ScrollView } from "react-native";
 import { useTranslation } from "react-i18next";
 
-import { PROJECTS } from "@assets";
-import { Project, Wrapper } from "@components";
+import { Project, ReAlert, Spinner, Wrapper } from "@components";
 import { Box, Text } from "@theme";
+import { useEffect, useState } from "react";
+import { getProjects } from "@api";
+import { ProjectItems } from "@types";
 
 export const Projects = () => {
+  const [projects, setProjects] = useState<Array<ProjectItems>>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+  const [displayError, setDisplayError] = useState<string>("");
+
   const { t } = useTranslation();
+
+  useEffect(() => {
+    getProjects()
+      .then((data) => {
+        setProjects(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setDisplayError(error);
+        setError(true);
+        setLoading(true);
+      });
+  }, []);
+
   return (
     <ScrollView>
       <Wrapper>
@@ -14,7 +35,10 @@ export const Projects = () => {
           {t("projects.title")}
         </Text>
         <Box marginVertical={"l"} testID="projects">
-          <Project project={PROJECTS.reverse()} />
+          {loading ? <Spinner /> : <Project project={projects} />}
+          {error && (
+            <ReAlert message={displayError} title={t("alert.error.title")} />
+          )}
         </Box>
       </Wrapper>
     </ScrollView>
