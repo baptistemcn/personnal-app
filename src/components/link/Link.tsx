443295837b523900b748fdd8071a9d6b7406473e
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-06 11:42:58 +0200

    refactor(link): add linking function

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,8 +1,7 @@
 import { TouchableOpacity } from "react-native-gesture-handler";
 
+import { openLink } from "@functions";
 import { LinkProps } from "@types";
-import { useCallback } from "react";
-import { Linking } from "react-native";
 
 export const Link = ({
   ariaLabel = "link",
@@ -10,13 +9,9 @@ export const Link = ({
   link = "https://github.com",
   testID = "link",
 }: LinkProps) => {
-  const handleClick = useCallback(async () => {
-    Linking.openURL(link);
-  }, [link]);
-
   return (
     <TouchableOpacity
-      onPress={handleClick}
+      onPress={() => openLink(link)}
       accessibilityLabel={ariaLabel}
       testID={testID}
     >
