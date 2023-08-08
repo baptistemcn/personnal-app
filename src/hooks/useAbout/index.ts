61bb622f52f972dbeda1c044ad890721936a4d09
Baptiste Marcon <baptistemcn@icloud.com>
2023-08-08 21:29:28 +0200

    feat(hooks): add useAbout

---
+++ src/hooks/useAbout/index.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+import { getAbout } from "@services";
+import { ExperienceItem } from "@types";
+
+export const useAbout = (lang: string) => {
+  const [about, setAbout] = useState<Array<ExperienceItem>>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+  const [displayError, setDisplayError] = useState<string>("");
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      await getAbout(lang)
+        .then((data) => {
+          setAbout(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setDisplayError(error);
+          setError(true);
+          setLoading(true);
+        });
+    };
+    fetchItems();
+  }, [lang]);
+
+  return { about, loading, error, displayError };
+};
